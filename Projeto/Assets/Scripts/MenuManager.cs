39a9e1227071d86c7a550a75553b534e27b5b224
JUH-MS <julianamoises@alunos.utfpr.edu.br>
2026-07-03 02:16:26 -0300

    Menu inicial completo

---
+++ Projeto/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject painelExplicacao;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void AbrirExplicacao()
+    {
+        painelExplicacao.SetActive(true);
+    }
+
+    public void FecharExplicacao()
+    {
+        painelExplicacao.SetActive(false);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+
+        // Apenas para testes no Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
